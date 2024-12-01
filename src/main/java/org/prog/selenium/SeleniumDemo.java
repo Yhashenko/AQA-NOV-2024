4c1f349330dcb535e157a82fd46da3a62d609f03
Юля <yulia.hashenkoqa@gmail.com>
2024-12-01 16:00:40 +0200

    Homework for session 6

--- src/main/java/org/prog/selenium/SeleniumDemo.java
+++ src/main/java/org/prog/selenium/SeleniumDemo.java
@@ -19,36 +19,36 @@ public class SeleniumDemo {
             // 1. open browser
             driver = new ChromeDriver();
             // 2. open google.com
-            driver.get("https://google.com/");
-            // 3. enter 'ben affleck' to search field
-            WebElement cookiesLink = driver.findElement(By.xpath("//a[contains(@href, 'technologies/cookies')]"));
-            if (cookiesLink.isDisplayed()) {
-                driver.findElements(By.tagName("button")).get(4).click();
-            }
-            WebElement searchInput = driver.findElement(By.name("q"));
-            searchInput.sendKeys("Ben Affleck");
+            driver.get("https://allo.ua/");
+            // 3. enter 'iphone 16 pro' to search field
+            // WebElement cookiesLink = driver.findElement(By.xpath("//a[contains(@href, 'technologies/cookies')]"));
+            // if (cookiesLink.isDisplayed()) {
+            //     driver.findElements(By.tagName("button")).get(4).click();
+            // }
+            WebElement searchInput = driver.findElement(By.name("search"));
+            searchInput.sendKeys("Apple iPhone 16 Pro");
             searchInput.sendKeys(Keys.ENTER);
 
-            List<WebElement> searchHeaders = new WebDriverWait(driver, Duration.ofSeconds(5L))
-                    .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.tagName("h3"), 1));
+            List<WebElement> searchHeaders = new WebDriverWait(driver, Duration.ofSeconds(10L))
+                    .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".product-card"), 1));
 
             int count = 0;
             for (WebElement webElement : searchHeaders) {
-                if (webElement.getText().contains("Ben Affleck")) {
+                if (webElement.getText().contains("Apple iPhone 16 Pro")) {
                     count++;
                 }
             }
 
-            if (count > 3) {
-                System.out.println("Ben Found!");
+            if (count > 1) {
+                System.out.println("Apple iPhone 16 Pro found!");
             } else {
-                System.out.println("Ben not found!");
+                System.out.println("Apple iPhone 16 Pro not found!");
             }
         } finally {
             if (driver != null) {
                 driver.quit();
             }
-        }
+       }
 
         // 4. press enter for search field
         // 5. check search results
